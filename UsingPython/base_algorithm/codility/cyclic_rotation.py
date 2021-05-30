61dd10dca0f2eac825507a162172cebb218b6bcc
Rick00Kim <dreamx119@gmail.com>
2021-05-31 01:14:07 +0900

    Copy Algorithm from SkillUp_Coding

---
+++ UsingPython/base_algorithm/codility/cyclic_rotation.py
@@ -0,0 +1,9 @@
+from base_algorithm.abstract_algorithm import AbstractAlgorithm
+
+
+class CyclicRotation(AbstractAlgorithm):
+    def solution(N):
+        pass
+
+    def execute(self):
+        return super().execute()
